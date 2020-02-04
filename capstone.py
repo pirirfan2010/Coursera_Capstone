6b2ba7b8007fa45bff3eb4d7b81952c4ddc1c368
pirirfan2010 <pirirfan2010@gmail.com>
2020-02-04 17:49:04 +0500

    this is just the testing of github

---
+++ capstone.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This is just the testing of github cloning and pushing project
+# which main task is to clone the your work on github 
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[1]:
+
+
+print("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
+
